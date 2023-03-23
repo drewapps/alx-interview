2fba953e4a6e6258797d374f8acc4161c8a86d1a
drewapps <druapps@gmail.com>
2023-03-24 09:39:37 +1000

    0x00-pascal_triangle first commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-'''
-Returns list representing the Pascal's triangle of n
-'''
+"""
+Pascale triangle
+"""
+
+
+def fact(n):
+    if n == 0:
+        return 1
+    prod = 1
+    for i in range(1, n+1):
+        prod = prod * i
+    return prod
 
 
 def pascal_triangle(n):
-    '''returns empty list if n <= 0'''
+    """
+    pascal triangle using python
+    """
     if n <= 0:
         return []
-
-    triangle = []
-    row = []
-    prev_row = []
-    for i in range(0, n + 1):
-        row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] +
-               prev_row[j] or 1 for j in range(0, i)]
-        prev_row = row
-        triangle += [row]
-    return triangle[1:]
-    
+    List_1 = []
+    for i in range(n):
+        List_2 = []
+        for j in range(i + 1):
+            List_2.append(fact(i)//(fact(j) * fact(i - j)))
+        List_1.append(List_2)
+    return List_1
