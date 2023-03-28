10aace70f9302fdb4e999e189eba07f45c089705
drewapps <druapps@gmail.com>
2023-03-28 23:14:08 +1000

    0x01-lockboxes first commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """ method that determines if all the boxes can be opened. """
-    n = len(boxes)
-    myList = [0]
-    for i in myList:
-        for j in boxes[i]:
-            if j not in myList:
-                if j < n:
-                    myList.append(j)
-    if len(myList) == n:
-        return True
-    return False
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
     
